a091f932ff78fd2ca225fe0fa954e11c6bf91eb7
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-08-14 12:55:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+const message = document.getElementById("text");
+const delayInSec = document.getElementById("delay");
+const submit = document.getElementById("btn");
+function getMessage(){
+	return new Promise((resolve)=>{
+		const text = message.value.trim();
+		const delay = parseInt(delayInSec.value);
+		setTimeout(()=>resolve(text),delay*1000);
+	});
+}
+async function showMessage(){
+	const message = await getMessage();
+	output.textContent = message;
+}
+submit.addEventListener("click",()=>{
+	showMessage();
+})
